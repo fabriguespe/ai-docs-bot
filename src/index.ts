978081eb03838d73b4b63994389ce2df2fde177d
fabri <fguespe@gmail.com>
2024-04-26 21:05:39 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,11 @@
 import "dotenv/config";
-import HandlerContext from "./lib/handler-context";
-import run from "./lib/runner.js";
-import fetch from "node-fetch";
 
-async function mockApiCall(query: string) {
-  try {
-    if (query == "Heartbeat") return "I'm alive";
-    if (process.env.DEBUG) console.log("Fetching from Kapa.ai API...");
-    const response = await fetch(
-      `${process.env.KAPA_API_ENDPOINT}?query=${encodeURIComponent(query)}`,
-      {
-        method: "GET",
-        headers: {
-          "X-API-TOKEN": process.env.KAPA_API_TOKEN as string,
-        },
-      }
-    );
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status}`);
-    }
-
-    const data = await response.json();
-    //@ts-ignore
-    const message = formatForPlainText(data?.answer);
-    console.log("Data from Kapa.ai API:", query, message);
-    return message;
-  } catch (error) {
-    console.error("Failed to fetch from Kapa.ai API:", error);
-    throw error;
-  }
-}
-function formatForPlainText(response: string): string {
-  // Remove Markdown links and directly capture URLs to list them later
-  let plainText = response.replace(/&lt;/g, "<").replace(/&gt;/g, ">");
-
-  plainText = plainText.replace(
-    /\[([^\]]+)\]\(([^)]+)\)/g,
-    (match, text, url) => url
-  );
-
-  // Extract and remove footnotes, capturing URLs to list them
-  const footnoteRegex = /\[\^(\d+)\]:\s*\[Source\]\(([^)]+)\)/g;
-  let match;
-  let sources = [];
-  while ((match = footnoteRegex.exec(response)) !== null) {
-    sources.push(match[2]); // Capture just the URL
-  }
-  plainText = plainText.replace(footnoteRegex, "");
-
-  // Remove remaining Markdown formatting (e.g., bold **text**)
-  plainText = plainText.replace(/\*\*([^*]+)\*\*/g, "$1");
-
-  return plainText;
-}
+import { run, HandlerContext } from "@xmtp/botkit";
+import { kapaApiCall } from "./lib/kapa.js";
 
 run(async (context: HandlerContext) => {
-  const { message } = context;
-
-  const { content, senderAddress } = message;
+  // Get the message and the address from the sender
+  const { content, senderAddress } = context.message;
 
   await new Promise((resolve) => setTimeout(resolve, 1000)); // Send initial thinking message
   await context.reply("Thinking...");
@@ -90,7 +38,7 @@ run(async (context: HandlerContext) => {
   }, intervalTime); // 6000 milliseconds = 6 seconds
 
   try {
-    const apiResponse = await mockApiCall(content);
+    const apiResponse = await kapaApiCall(content);
     clearInterval(thinkingInterval); // Stop the periodic messages
     await context.reply(apiResponse);
   } catch (error) {
